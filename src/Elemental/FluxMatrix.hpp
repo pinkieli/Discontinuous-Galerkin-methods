dca6c09f8fe58086e8a508a50e44f935c06ca537
Kaushik Kulkarni <kaushikcfd@gmail.com>
2016-03-25 17:00:57 +0530

    Made the 2D flux Matrices.

--- src/Elemental/FluxMatrix.hpp
+++ src/Elemental/FluxMatrix.hpp
@@ -1,5 +1,7 @@
 #include <lapacke.h>
 #include "../Utilities/LagrangePolynomials.hpp"
+#include "../Utilities/Zeros.hpp"
+#include "MassMatrix.hpp"
 
 #ifndef FluxMatrix_HPP
 #define FluxMatrix_HPP
@@ -29,14 +31,64 @@ void fluxMatrix(double *FluxMatrix, unsigned N)
     }
 
     for(i=0;i<=N;i++)
-    {
         delete[] poly[i];
-    }
 
     delete[] poly;
-
     return ;
 }
 
+void twoDFluxMatrix1(double *Flux1, unsigned N)
+{
+    unsigned i1,i2,j1,j2;
+    double M[N+1][N+1];
+    massMatrix(*M,N);
+    zeros(Flux1,(N+1)*(N+1),(N+1)*(N+1));
+
+    i1=i2=0;
+
+    for(j1=0;j1<=N;j1++)
+            for(j2=0;j2<=N;j2++)
+                Flux1[(i1*(N+1)+j1)*(N+1)*(N+1)+i2*(N+1)+j2] = M[j1][j2];
+}
+
+void twoDFluxMatrix3(double *Flux3, unsigned N)
+{
+    unsigned i1,i2,j1,j2;
+    double M[N+1][N+1];
+    massMatrix(*M,N);
+    zeros(Flux3,(N+1)*(N+1),(N+1)*(N+1));
+
+    i1=i2=N;
+
+    for(j1=0;j1<=N;j1++)
+            for(j2=0;j2<=N;j2++)
+                Flux3[(i1*(N+1)+j1)*(N+1)*(N+1)+i2*(N+1)+j2] = M[j1][j2];
+}
+
+void twoDFluxMatrix2(double *Flux2, unsigned N)
+{
+    unsigned i1,i2,j1,j2;
+    double M[N+1][N+1];
+    massMatrix(*M,N);
+    zeros(Flux2,(N+1)*(N+1),(N+1)*(N+1));
+
+    j1=j2=N;
+    for(i1=0;i1<=N;i1++)
+        for(i2=0;i2<=N;i2++)
+                Flux2[(i1*(N+1)+j1)*(N+1)*(N+1)+i2*(N+1)+j2] = M[i1][i2];
+}
+
+void twoDFluxMatrix4(double *Flux4, unsigned N)
+{
+    unsigned i1,i2,j1,j2;
+    double M[N+1][N+1];
+    massMatrix(*M,N);
+    zeros(Flux4,(N+1)*(N+1),(N+1)*(N+1));
+
+    j1=j2=0;
+    for(i1=0;i1<=N;i1++)
+        for(i2=0;i2<=N;i2++)
+                Flux4[(i1*(N+1)+j1)*(N+1)*(N+1)+i2*(N+1)+j2] = M[i1][i2];
+}
 
 #endif
