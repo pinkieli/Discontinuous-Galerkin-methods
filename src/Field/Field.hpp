c742b83f58f51a4d0ec156734ef9b56164d65f29
Kaushik Kulkarni <kaushikcfd@gmail.com>
2016-03-26 09:22:31 +0530

    The `Field` class is updated. The time integrator step is remaining.

--- src/Field/Field.hpp
+++ src/Field/Field.hpp
@@ -2,10 +2,16 @@
 #include <functional>
 #include <cstring>
 #include <cblas.h>
+#include <cmath>
 #include "../Utilities/LobattoNodes.hpp"
 #include "../Utilities/Inverse.hpp"
 #include "../Elemental/MassMatrix.hpp"
 #include "../Elemental/DerivativeMatrix.hpp"
+#include "../Elemental/FluxMatrix.hpp"
+
+#define MAX(a, b)(a>b?a:b)
+#define ABS(a)(a>0?a:(-a))
+
 using namespace std;
 
 #ifndef Field_HPP
@@ -17,12 +23,14 @@ private:
     unsigned Nex,Ney,N;
     double L_start,L_end,H_start,H_end;
 
-    double ***ConsVariable,***Rate;
+    double ***ConsVariable,***Rate,***XFlux,***YFlux;
     double ***X, ***Y;
     double ***U, ***V;
 
     double dt;
     unsigned NTimeSteps;
+
+    double Lambda;
 
 public:
 
@@ -32,7 +40,8 @@ public:
     void setVelocity(function<double(double,double)> , function<double(double,double)> );
     void setInitialConditions(function<double(double,double)> );
     void setSolver(double , unsigned );
-
+    void computeLambda();
+    void computeFluxTerms(double ***, double ***);
     void solve();
 
 };
@@ -57,6 +66,22 @@ Field::Field(unsigned Nx, unsigned Ny, unsigned n)
         Rate[i]     =   new double* [Nex];
         for(j=0;j<Nex;j++)
             Rate[i][j]      =   new double[(N+1)*(N+1)];
+    }
+
+    XFlux           =   new double** [Ney];
+    for( i = 0; i< Ney; i++)
+    {
+        XFlux[i]    =   new double* [Nex];
+        for(j=0;j<Nex;j++)
+            XFlux[i][j]     =   new double[(N+1)*(N+1)];
+    }
+
+    YFlux           =   new double** [Ney];
+    for( i = 0; i< Ney; i++)
+    {
+        YFlux[i]    =   new double* [Nex];
+        for(j=0;j<Nex;j++)
+            YFlux[i][j]     =   new double[(N+1)*(N+1)];
     }
 
     X    =   new double** [Ney];
@@ -157,14 +182,97 @@ void Field::setSolver(double a, unsigned b)
     NTimeSteps  =   b;
 }
 
+void Field::computeLambda()
+{
+    Lambda = 0;
+    unsigned i,j,k;
+    for(i=0;i<Ney;i++)
+    {
+        for(j=0;j<Nex;j++)
+        {
+            for(k=0;k<=N;k++)
+            {
+                Lambda  =   MAX(Lambda,ABS(V[i][j][k]));
+                Lambda  =   MAX(Lambda,ABS(U[i][j][k*(N+1)]));
+            }
+        }
+    }
+
+    j   =   Nex-1;
+
+    for(i=0;j<Ney;i++)
+        for(k=0;k<=N;k++)
+            Lambda  =   MAX(Lambda,ABS(U[i][j][k*(N+1)+N]));
+
+    i   =   Ney-1;
+
+    for(j=0;j<Nex;j++)
+        for(k=0;k<=N;k++)
+            Lambda  =   MAX(Lambda,ABS(V[i][j][k + N*(N+1) ]));
 
 
+    return ;
+}
+
+void Field::computeFluxTerms(double ***f_preX, double ***f_preY )
+{
+    unsigned i,j,k;
+
+    for(i=0;i<Ney;i++)
+    {
+        for(j=1;j<Nex;j++)
+        {
+            for(k=0;k<=N;k++)
+            {
+                XFlux[i][j][k*(N+1)]    =   0.5*(f_preX[i][j][k*(N+1)] + f_preX[i][j-1][k*(N+1) + N] - Lambda*(ConsVariable[i][j][k*(N+1)]-ConsVariable[i][j-1][k*(N+1)+N]) );
+                XFlux[i][j-1][k*(N+1)+N]=   0.5*(f_preX[i][j][k*(N+1)] + f_preX[i][j-1][k*(N+1) + N] - Lambda*(ConsVariable[i][j][k*(N+1)]-ConsVariable[i][j-1][k*(N+1)+N]) );
+            }
+        }
+    }
+
+    j=0;
+
+    for(i=0;i<Ney;i++)
+    {
+        for(k=0;k<=N;k++)
+        {
+            XFlux[i][j][k*(N+1)]        =   0.5*(f_preX[i][j][k*(N+1)] + f_preX[i][Nex-1][k*(N+1) + N] - Lambda*(ConsVariable[i][j][k*(N+1)]-ConsVariable[i][Nex-1][k*(N+1)+N]) );
+            XFlux[i][Nex-1][k*(N+1)+N]  =   0.5*(f_preX[i][j][k*(N+1)] + f_preX[i][Nex-1][k*(N+1) + N] - Lambda*(ConsVariable[i][j][k*(N+1)]-ConsVariable[i][Nex-1][k*(N+1)+N]) );
+        }
+    }
+
+    for(i=1;i<Ney;i++)
+    {
+        for(j=0;j<Nex;j++)
+        {
+            for(k=0;k<=N;k++)
+            {
+                YFlux[i][j][k]              =   0.5*(f_preY[i][j][k] + f_preY[i-1][j][k + N*(N+1)] - Lambda*(ConsVariable[i][j][k] - ConsVariable[i-1][j][k + N*(N+1)]) );
+                YFlux[i-1][j][k + N*(N+1)]  =   0.5*(f_preY[i][j][k] + f_preY[i-1][j][k + N*(N+1)] - Lambda*(ConsVariable[i][j][k] - ConsVariable[i-1][j][k + N*(N+1)]) );
+            }
+        }
+    }
+
+    i=0;
+
+    for(j=0;j<Nex;j++)
+    {
+        for(k=0;k<=N;k++)
+        {
+            YFlux[i][j][k]                  =   0.5*(f_preY[i][j][k] + f_preY[Ney-1][j][k + N*(N+1)] - Lambda*(ConsVariable[i][j][k] - ConsVariable[Ney-1][j][k + N*(N+1)]) );
+            YFlux[Ney-1][j][k + N*(N+1)]    =   0.5*(f_preY[i][j][k] + f_preY[Ney-1][j][k + N*(N+1)] - Lambda*(ConsVariable[i][j][k] - ConsVariable[Ney-1][j][k + N*(N+1)]) );
+        }
+    }
+
+    return ;
+}
 
 void Field::solve()
 {
     unsigned i,j,k;
     double MassMatrix[(N+1)*(N+1)][(N+1)*(N+1)];
     double MassInverse[(N+1)*(N+1)][(N+1)*(N+1)];
+    double Flux1[(N+1)*(N+1)][(N+1)*(N+1)],Flux2[(N+1)*(N+1)][(N+1)*(N+1)],Flux3[(N+1)*(N+1)][(N+1)*(N+1)],Flux4[(N+1)*(N+1)][(N+1)*(N+1)];
     double DerivativeMatrixX[(N+1)*(N+1)][(N+1)*(N+1)], DerivativeMatrixY[(N+1)*(N+1)][(N+1)*(N+1)];
 
     twoDMassMatrix(*MassMatrix,N);
@@ -173,9 +281,13 @@ void Field::solve()
     twoDDerivativeMatrixX(*DerivativeMatrixX,N);
     twoDDerivativeMatrixY(*DerivativeMatrixY,N);
 
-    double ***RHS, ***PreviousValue, ***f_preX,***f_preY;
+    twoDFluxMatrix1(*Flux1,N);
+    twoDFluxMatrix2(*Flux2,N);
+    twoDFluxMatrix3(*Flux3,N);
+    twoDFluxMatrix4(*Flux4,N);
+
+    double ***RHS, ***f_preX,***f_preY;
     RHS             =   new double** [Ney];
-    PreviousValue   =   new double** [Ney];
     f_preX          =   new double** [Ney];
     f_preY          =   new double** [Ney];
     for( i = 0; i< Ney; i++)
@@ -183,13 +295,11 @@ void Field::solve()
         f_preX[i]           =   new double* [Nex];
         f_preY[i]           =   new double* [Nex];
         RHS[i]              =   new double* [Nex];
-        PreviousValue[i]    =   new double* [Nex];
         for(j=0;j<Nex;j++)
         {
             f_preX[i][j]        =   new double[(N+1)*(N+1)];
             f_preY[i][j]        =   new double[(N+1)*(N+1)];
             RHS[i][j]           =   new double[(N+1)*(N+1)];
-            PreviousValue[i][j] =   new double[(N+1)*(N+1)];
         }
     }
 
@@ -198,24 +308,34 @@ void Field::solve()
         for(j=0;j<Nex;j++)
         {
             memcpy(RHS[i][j],ConsVariable[i][j],(N+1)*(N+1)*sizeof(double));
-            memcpy(PreviousValue[i][j],ConsVariable[i][j],(N+1)*(N+1)*sizeof(double));
             for(k=0;k<(N+1)*(N+1);k++)
             {
-                f_preX[i][j][k]  = PreviousValue[i][j][k]*U[i][j][k];
-                f_preY[i][j][k]  = PreviousValue[i][j][k]*V[i][j][k];
+                f_preX[i][j][k]  = ConsVariable[i][j][k]*U[i][j][k];
+                f_preY[i][j][k]  = ConsVariable[i][j][k]*V[i][j][k];
             }
             cblas_dgemv(CblasRowMajor,CblasTrans,(N+1)*(N+1),(N+1)*(N+1),1,*DerivativeMatrixX,(N+1)*(N+1),f_preX[i][j],1,0,RHS[i][j],1);
             cblas_dgemv(CblasRowMajor,CblasTrans,(N+1)*(N+1),(N+1)*(N+1),1,*DerivativeMatrixY,(N+1)*(N+1),f_preY[i][j],1,1,RHS[i][j],1);
         }
     }
 
-    /*HERE FLUX WILL DO THE CHANGES TO THE RHS USING THE PREVIOUS VALUE AND RHS*/
-    /*What I am planning right now is continue to build up the flux matrices as in the previous case and then figure out a way to deal with the non - contiguity.*/
-    /*OR there could be a way of iterating through the sides and getting the result like done by Giraldo.*/
+    computeLambda();
+    computeFluxTerms(f_preX, f_preY);
+
+    for(i=0;i<Ney;i++)
+    {
+        for(j=0;j<Nex;j++)
+        {
+            cblas_dgemv(CblasRowMajor,CblasNoTrans,(N+1)*(N+1),(N+1)*(N+1),-1,*Flux2,(N+1)*(N+1),XFlux[i][j],1,0,RHS[i][j],1);
+            cblas_dgemv(CblasRowMajor,CblasNoTrans,(N+1)*(N+1),(N+1)*(N+1), 1,*Flux4,(N+1)*(N+1),XFlux[i][j],1,0,RHS[i][j],1);
+            cblas_dgemv(CblasRowMajor,CblasNoTrans,(N+1)*(N+1),(N+1)*(N+1),-1,*Flux3,(N+1)*(N+1),YFlux[i][j],1,0,RHS[i][j],1);
+            cblas_dgemv(CblasRowMajor,CblasNoTrans,(N+1)*(N+1),(N+1)*(N+1), 1,*Flux1,(N+1)*(N+1),YFlux[i][j],1,0,RHS[i][j],1);
+        }
+    }
+
 
     for(i=0;i<Ney;i++)
         for(j=0;j<Nex;j++)
-            cblas_dgemv(CblasRowMajor,CblasTrans,(N+1)*(N+1),(N+1)*(N+1),1,*MassInverse,(N+1)*(N+1),RHS[i][j],1,0,Rate[i][j],1);
+            cblas_dgemv(CblasRowMajor,CblasNoTrans,(N+1)*(N+1),(N+1)*(N+1),1,*MassInverse,(N+1)*(N+1),RHS[i][j],1,0,Rate[i][j],1);
 
     return ;
 }
