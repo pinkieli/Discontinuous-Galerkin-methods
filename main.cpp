847d457fc09690af72791b44c7b47325812374ff
Kaushik Kulkarni <kaushikcfd@gmail.com>
2016-05-30 10:09:29 +0530

    Added a feature for 2D plots.

--- main.cpp
+++ main.cpp
@@ -3,54 +3,25 @@
 #include <string>
 using namespace std;
 
-
-double U(double x, double y)
-{
-    return 0.0;
+double f(double x) {
+    return cos(x);
 }
 
-double V(double x, double y)
-{
-    return 0.0;
-}
-
-double eta(double x, double y)
-{
-    return (exp(-16*(x*x+y*y)));
-}
-
-double Depth(double x, double y)
-{
-    return 2.0;
-}
-int main()
-{
-    string name;
-    unsigned    Nex =   20;
-    unsigned    Ney =   20;
-    unsigned    N   =   4 ;
-    double L_start  =   -2;
-    double L_end    =   2;
-    double H_start  =   -2;
-    double H_end    =   2;
-    unsigned NTimeSteps =200;
-    double dt  =   1e-5;
-    unsigned i=0;
-
-
-    ShallowWater q(Nex,Ney,N);
-    q.setDomain(L_start,L_end,H_start,H_end);
-    q.setDepth(Depth);
-    q.setInitialConditions(eta,U,V);
-    q.setSolver(dt,NTimeSteps);
-    for(i=0;i<50;i++)
-    {
-        name =  "t="+to_string(i*NTimeSteps*dt)+"s";
-        q.plotSolution(0.0,3.0,name);
-        q.solve();
+int main()  {
+    unsigned N = 1000;
+    double L_start  =   -3.14159;
+    double L_end    =    3.14159;
+    double h        =   (L_end-L_start)/N;
+    unsigned i;
+    double X[N],Y[N];
+    X[0]    =   L_start;
+    Y[0]    =   f(X[0]);
+    for(i=1;i<N;i++) {
+        X[i]    =   X[i-1]  +   h;
+        Y[i]    =   f(X[i]);
     }
-    name =  "t="+to_string(i*NTimeSteps*dt)+"s";
-    q.plotSolution(0.0,3.0,name);
+
+    plot(X,Y,N,"Try","Density","CoSine");
 
     return 0;
 }
--- src/Utilities/Plot.hpp
+++ src/Utilities/Plot.hpp
@@ -7,6 +7,42 @@ using namespace std;
 #ifndef Plot_HPP
 #define Plot_HPP
 
+
+void plot(double *X, double *Y, double N, string GraphTitle = "", string LegendTitle = "", string fileName = "")
+{
+    unsigned i;
+    FILE* pfile;
+    /*Starting the dumping of data to the file `temp.dat`*/
+    pfile=freopen("temp.dat","w",stdout);
+        printf("# X\tY\n");
+        for(i=0; i<N; i++)
+            printf("%.6f\t%.6f\n",X[i],Y[i]);
+    fclose(stdout);
+    /*Writing the file for the GNUPLOT*/
+    pfile=freopen("temp.gnu","w",stdout);
+        printf("reset\n");
+        printf("set terminal jpeg interlace enhanced size 1366,768\n");
+        printf("set output \"%s.jpg\"\n", fileName.c_str());
+        printf("set key right box\n");
+        printf("set grid\n");
+        printf("set title '%s'\n",GraphTitle.c_str());
+        printf("set ylabel 'Y'\n");
+        printf("set xlabel 'X'\n");
+        printf("plot 'temp.dat' title '%s' w lines ls 1\n",LegendTitle.c_str());
+    fclose(stdout);
+    /*Writing commands to the terminal for making the plot.*/
+    pfile=freopen ("/dev/tty", "a", stdout);
+    i=system("gnuplot> load 'temp.gnu'");
+    i=system("rm -rf temp* load");
+    return ;
+}
+
+/**
+ * This is a plot function which takes three 2D arrays and makes the surface plots of them.
+ * @param X    The X-Domain.
+ * @param Y    The Y-Domain.
+ * @param Z    The Z-Domain.
+ */
 void plot(double *X, double *Y, double *Z, unsigned Nx, unsigned Ny,double X1, double X2,double Y1, double Y2,double Z1, double Z2, string Name)
 {
     int i;
